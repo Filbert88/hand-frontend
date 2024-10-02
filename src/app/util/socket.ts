1a478630e8083ae93ad99528516d6d3752bcf094
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-10-03 01:05:10 +0700

    fix : fix chat feature

--- src/app/util/socket.ts
+++ src/app/util/socket.ts
@@ -1,5 +1,9 @@
-export const createWebSocket = (url: string) => {
-  const ws = new WebSocket(url);
+import { getToken } from "@/utils/function";
+
+export const createWebSocket = async (url: string) => {
+  const token = await getToken();
+  const ws = new WebSocket(`${url}?token=${token}`);
+
 
   ws.onopen = () => {
     console.log("Connected to WebSocket");
