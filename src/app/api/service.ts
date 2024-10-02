680a8ebfde65ff5236d1b6ba40e3365cb31e9cba
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-10-02 15:59:04 +0700

    feat: add service

--- src/app/api/service.ts
+++ src/app/api/service.ts
@@ -1,3 +1,5 @@
+import { ChatMessage } from "../find/[roomId]/page";
+
 export interface CheckInData {
   mood_score: number;
   feelings: string;
@@ -69,5 +71,27 @@ export async function updateCheckIn(checkInData: CheckInData) {
     }
   } catch (error) {
     console.error("Error:", error);
+  }
+}
+
+export async function fetchChatMessages(roomId: string): Promise<ChatMessage[] | undefined> {
+  try {
+    const response = await fetch(`http://localhost:8080/api/room/message/${roomId}`, {
+      method: "GET",
+      credentials: "include", // Ensure cookies for session management are included with the request
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    if (response.ok) {
+      console.log("Chat Messages Fetched:", data);
+      return data as ChatMessage[];
+    } else {
+      throw new Error(data.message || "Error fetching chat messages");
+    }
+  } catch (error) {
+    console.error("Fetch Chat Messages Error:", error);
+    return undefined;
   }
 }
