15ce4432749b8e9a4dc30480b8c8d8f1851a554b
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-10-03 22:34:25 +0700

    feat: add coming soon page

---
+++ src/app/coming-soon/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function ComingSoonPage() {
+  return (
+    <div
+      className="min-h-screen flex justify-center items-center"
+      style={{
+        background: 'linear-gradient(120deg, #FECFD7, #F9EBE2, #D6F4EC, #F2E5F9)',
+      }}
+    >
+      <div className="text-center">
+        <h1 className="text-4xl sm:text-6xl font-bold text-black">
+          We have something great prepared!
+        </h1>
+        <p className="mt-4 text-3xl sm:text-4xl font-bold text-black">
+          Coming soon! (´｡• ◡ •｡`) ♡
+        </p>
+      </div>
+    </div>
+  );
+}
